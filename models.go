c810c09c3f3fcc33af99ae40ea41630d58e0037c
Orozco David <dvdizcky@gmail.com>
2020-10-07 15:13:13 -0500

    config: model added

---
+++ models.go
@@ -0,0 +1,23 @@
+package configuration
+
+type Configuration struct {
+	internalBrowsers map[string]Browser
+	filename         string
+	Browsers         []Browser `json:"browsers,omitempty"`
+	Port             string    `json:"port,omitempty"`
+	Owner            struct {
+		Home     string `json:"home,omitempty"`
+		Username string `json:"username,omitempty"`
+	} `json:"owner,omitempty"`
+	InternalStatus struct {
+		Logged            bool   `json:"logged,omitempty"`
+		ConfigurationFile string `json:"configuration_file,omitempty"`
+		CredentialsFile   string `json:"credentials_file,omitempty"`
+	} `json:"internal_status,omitempty"`
+}
+
+type Browser struct {
+	Name    string `json:"name,omitempty"`
+	Version string `json:"version,omitempty"`
+	Path    string `json:"path,omitempty"`
+}
