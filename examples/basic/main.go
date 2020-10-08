a55891935031b0ad4893cf59c07f9012a553c313
Orozco David <dvdizcky@gmail.com>
2020-10-08 10:34:18 -0500

    examples: basic example using the new api

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/hippokampe/configuration"
+	"github.com/hippokampe/configuration/credentials"
 	"log"
 )
 
 func main() {
-	config := configuration.New("/etc/hbtn/general.json")
+	config := configuration.New("general.json")
 	if err := config.ReadGeneralConfig(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(config.SetUsername("root", true))
-	fmt.Println(config.SetUsername("davixcky", false))
-	fmt.Println(config.GetPort())
+	config.InternalStatus.Credentials = &credentials.Credentials{
+		ID: "1234",
+		Email: "email",
+		CredentialsFile: "credentials.json",
+	}
+
+	fmt.Println(config.InternalStatus.Credentials.Save())
+
+	fmt.Println(config.SetLogged())
+	if err := config.WriteConfig(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(config.IsLogged())
+
 	if err := config.WriteConfig(); err != nil {
 		log.Fatal(err)
 	}
